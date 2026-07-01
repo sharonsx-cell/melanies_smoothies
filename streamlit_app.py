43394ed468c5e98874655d322b53f4ccfa1b4018
sharonsx-cell <sharon.sx.leung@gmail.com>
2026-07-01 21:57:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-
+import pandas
 
 # Write directly to the app
 st.title(f":cup_with_straw: Example Streamlit App :cup_with_straw: ")
